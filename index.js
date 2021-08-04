12021854be4b52c88ef9487e1d295daed1090184
Gokul665665 <agokul.doodleblue@gmail.com>
2021-08-04 22:08:58 +0530

    entered the code in index.html and index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",(req,res)=> {
+    var height = req.body.Height 
+    var weight = req.body.Weight 
+    var BMI = Weight / Height * Height 
+    res.send("the calculated BMI is" + BMI );
+});
+
+app.listen(3000,()=>{
+    console.log("server is active in the port : 3000");
+});
